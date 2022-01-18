5773efd65dad11c9982442c7001a85657867b7ec
Zhicong Zeng <zzeng1@seattleu.edu>
2022-01-17 18:09:07 -0800

    Finished all and added some UnitTests

--- src/Tree/TreeEvaluator.cs
+++ src/Tree/TreeEvaluator.cs
@@ -24,6 +24,88 @@ namespace RecDescent.Tree
         // for an AddOperator).
         private void EvaluateImpl(TokenTreeNode node, Dictionary<string, int> variables)
         {
+            //Like an XML need to parser every node
+            if (node.TokenType == Goal)
+            {
+                EvaluateImpl(node.Children[0], variables);
+                return;
+            }
+            if (node.TokenType == Expr)
+            {
+                EvaluateImpl(node.Children[0], variables);
+                EvaluateImpl(node.Children[1], variables);
+                return;
+            }
+            if (node.TokenType == Term)
+            {
+                EvaluateImpl(node.Children[0], variables);
+                EvaluateImpl(node.Children[1], variables);
+                return;
+            }
+            if (node.TokenType == Factor)
+            {
+                if(node.Children.Count == 0)    return;
+
+                if (node.Children[0].TokenType == Identifier)
+                {
+                    stack.Push(variables[node.Children[0].Lexeme]);
+                    return;
+                }
+                if (node.Children[0].TokenType == Number)
+                {
+                    stack.Push(int.Parse(node.Children[0].Lexeme));
+                    return;
+                }
+            }
+            if (node.TokenType == TermPrime)
+            {
+                if (node.Children.Count == 0) return;
+
+                if (node.Children[0].TokenType == MulOperator)
+                {
+                    node = node.Children[0];
+                    EvaluateImpl(node.Children[0], variables);
+                    stack.Push(stack.Pop() * stack.Pop());
+                    return;
+                }
+                if (node.Children[0].TokenType == DivOperator)
+                {
+                    node = node.Children[0];
+                    EvaluateImpl(node.Children[0], variables);
+                    int temp = stack.Pop();
+                    stack.Push(stack.Pop() / temp);
+                    return;
+                }
+            }
+            if (node.TokenType == ExprPrime)
+            {
+                if (node.Children.Count == 0) return;
+
+                if (node.Children[0].TokenType == AddOperator)
+                {
+                    node = node.Children[0];
+                    EvaluateImpl(node.Children[0], variables);
+                    EvaluateImpl(node.Children[1], variables);
+                    stack.Push(stack.Pop() + stack.Pop());
+                    return;
+                }
+                if (node.Children[0].TokenType == SubOperator)
+                {
+                    node = node.Children[0];
+                    EvaluateImpl(node.Children[0], variables);
+                    EvaluateImpl(node.Children[1], variables);
+                    stack.Push(-(stack.Pop() - stack.Pop()));
+                    return;
+                }
+                if (node.Children[0].TokenType == LeftParen)
+                {
+                    node = node.Children[0];
+                    EvaluateImpl(node.Children[0], variables);
+                    EvaluateImpl(node.Children[1], variables);
+                    return;
+                }
+            }
+            return;
         }
     }
 }
